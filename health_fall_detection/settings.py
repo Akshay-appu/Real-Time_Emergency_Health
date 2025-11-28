5ebd28137ddf7606851c312f3ac905a6617916d8
Akshay-appu <appu03626@gmail.com>
2025-11-28 17:53:09 +0530

    Update settings.py

--- health_fall_detection/settings.py
+++ health_fall_detection/settings.py
@@ -3,7 +3,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'change-me-to-a-secure-random-string'
 DEBUG = True
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ["*", "health-fall-detection.onrender.com"]
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
